7c6e2104c8c0ea35479a3f28e102ed3b4408a580
jocelyncheah <jocelyncheah@gmail.com>
2019-06-13 01:16:47 +0000

    finally completed this, but need to practice using new method

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,6 +1,6 @@
 from .models import Gist
-import datetime
-
+from datetime import datetime
+"""
 def search_gists(db_connection, **kwargs):
     query = 'Select * from gists'
     if not kwargs:
@@ -19,7 +19,7 @@ def search_gists(db_connection, **kwargs):
         }
 
         for k, v in kwargs.items(): 
-            if type(v) == datetime.datetime:
+            if isinstance(v, datetime):
                 if '__' not in k:
                     operator = '='
                 else:
@@ -45,4 +45,57 @@ def search_gists(db_connection, **kwargs):
     ################    
     results = [Gist(row) for row in cursor]
     return results  
-     
+"""
+    
+
+def comparison(operator):
+    return {
+        'lt': '<',
+        'lte': '<=',
+        'gt': '>',
+        'gte': '>=',
+    }[operator]
+
+def search_gists(db_connection, **kwargs):
+
+
+#     if 'github_id' in kwargs:
+#         cursor = db_connection.execute('SELECT * FROM gists WHERE github_id = :github_id', kwargs)
+#         return [Gist(gist) for gist in cursor]
+# #         return cursor.fetchall()
+#     if 'created_at' in kwargs:
+#         cursor = db_connection.execute('SELECT * FROM gists WHERE datetime(created_at) == datetime(:created_at)', kwargs)
+#         return [Gist(gist) for gist in cursor]
+# #         return cursor.fetchall()
+#     else:
+#         cursor = db_connection.execute('SELECT * FROM gists')
+#         return cursor.fetchall()
+
+    query = 'SELECT * FROM gists'
+    params = {}
+    if kwargs:
+        filters = []
+        for param, value in kwargs.items():
+            if param.startswith(('created_at', 'updated_at')):
+                if '__' in param:
+                    attribute, operator = param.split('__')
+                    oper = comparison(operator)
+                    filters.append('datetime({}) {} datetime(:{})'.format(attribute, oper, param))
+
+                else:
+                    attribute = param
+                    filters.append('datetime({}) == datetime(:{})'.format(attribute, param))
+                params[param] = value
+
+            else:
+                filters.append('%s = :%s' % (param, param))
+                params[param] = value
+
+        query += ' WHERE '
+        query += ' AND '.join(filters)
+        cursor = db_connection.execute(query, params)
+    else:
+        cursor = db_connection.execute(query)
+
+    results = [Gist(gist) for gist in cursor]
+    return results  
